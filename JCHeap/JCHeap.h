6d64ac03250be9252f829f8aafb15f806d2e6d09
Jonathan Crooke <joncrooke@gmail.com>
2014-12-26 12:31:43 +0000

    Initial Commit

---
+++ JCHeap/JCHeap.h
@@ -0,0 +1,13 @@
+//
+//  JCHeap.h
+//  JCHeap
+//
+//  Created by Jonathan Crooke on 26/12/2014.
+//  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCHeap : NSObject
+
+@end
---
+++ JCHeap/JCHeap.m
@@ -0,0 +1,13 @@
+//
+//  JCHeap.m
+//  JCHeap
+//
+//  Created by Jonathan Crooke on 26/12/2014.
+//  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
+//
+
+#import "JCHeap.h"
+
+@implementation JCHeap
+
+@end
